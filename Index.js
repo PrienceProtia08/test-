ff8e2e99da533bc300299f60d009e61f3462b352
PrienceProtia08 <99395410+PrienceProtia08@users.noreply.github.com>
2022-02-10 17:51:12 +0800

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,12 +1,22 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-const port = 3000
+const port = 3000 || 8000 || 5500;
 
-app.get('/', (req, res) => res.send('Yo boi!!'))
+app.get('/', (req ,res) => {
+  res.send("Bot is online")
+});
 
-app.listen(port, () =>
-console.log(`Your app is listening a http://localhost:${port}`)
-);
+app.listen(port, () => {
+  console.log(`Server listening at https://localhost/${port}`)
+})
+
+client.login(process.env.TOKEN);
+client.commands = new Collection();
+client.prefix = PREFIX;
+client.queue = new Map();
+const cooldowns = new Collection();
+const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+const {BOT_PREFIX, BOT_STATUS2, BOT_STATUS3 , BOT_PRESENCE } = require('./config.json');
 
 Const Discord = require("discord.js);
 Module.exports = {
@@ -24,3 +34,10 @@ Let db = require("quick.db")
 
 db.set("fun", guy) 
      message.channel.send("DONE")
+     
+     const { Client, Collection } = require("discord.js");
+const { readdirSync } = require("fs");
+const { join } = require("path");
+const { PREFIX } = require("./config.json");
+const client = new Client({ disableMentions: "everyone" })
+
